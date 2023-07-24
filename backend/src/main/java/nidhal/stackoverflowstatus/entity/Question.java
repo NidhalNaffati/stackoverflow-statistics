b8db9c0085eea97b1b9c51c9a4211f03fbcbf4f9
NidhalNaffati <nidhalnaffati@gmail.com>
2023-07-24 18:53:33 +0100

    feat: Implement comprehensive test suite for the QuestionDao.
    In this commit, I have introduced the QuestionsDaoTest class to thoroughly test all the methods within the QuestionService, achieving 100% test coverage and 87% line coverage. Additionally, to enable its usage in the test class, I have created the equals method allowing comparison between questions.

--- backend/src/main/java/nidhal/stackoverflowstatus/entity/Question.java
+++ backend/src/main/java/nidhal/stackoverflowstatus/entity/Question.java
@@ -5,6 +5,7 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Builder
 @Entity
@@ -34,4 +35,24 @@ public class Question implements Serializable {
     public long closed_date;
     public long last_activity_date;
     public long last_edit_date;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question question)) return false;
+        return getQuestion_id() == question.getQuestion_id() &&
+               getScore() == question.getScore() &&
+               getAnswer_count() == question.getAnswer_count() &&
+               getAccepted_answer_id() == question.getAccepted_answer_id() &&
+               getView_count() == question.getView_count() &&
+               getCreation_date() == question.getCreation_date() &&
+               getClosed_date() == question.getClosed_date() &&
+               getLast_activity_date() == question.getLast_activity_date() &&
+               getLast_edit_date() == question.getLast_edit_date() &&
+               Objects.equals(getIs_answered(), question.getIs_answered()) &&
+               Objects.equals(getTitle(), question.getTitle()) &&
+               Objects.equals(getLink(), question.getLink()) &&
+               Objects.equals(getTags(), question.getTags());
+    }
+
 }
