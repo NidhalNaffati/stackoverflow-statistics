956062b40703387b86492b9298d1d336ea99cbb3
NidhalNaffati <nidhalnaffati@gmail.com>
2023-08-05 13:39:49 +0100

    fix: CORS configuration issue
    In this commit I have created the `WebMvcConfig` class responsible for setting the configuration of the CORS.
    
    By resolving this CORS issue, the application should have improved cross-origin support and a more reliable communication mechanism with the frontend.

--- backend/src/main/java/nidhal/stackoverflowstatus/configuration/ApplicationConfig.java
+++ backend/src/main/java/nidhal/stackoverflowstatus/configuration/ApplicationConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-
 public class ApplicationConfig {
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
