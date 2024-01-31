3f55ca1612ec6ecf6e90b3c64d684653d94f4ff9
NidhalNaffati <nidhalnaffati@gmail.com>
2024-01-31 21:27:23 +0100

    feat: implement environment variables for VITE_SPRING_APP_URL

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,8 +1,12 @@
 import axios from 'axios'
 
+const springBootAPI = import.meta.env.VITE_SPRING_APP_URL + '/api/v1/questions'
+
 const axiosInstance = axios.create({
   // Replace with the actual Spring Boot API URL
-  baseURL: 'http://localhost:8080/api/v1/questions'
+  baseURL: springBootAPI
 })
+
+console.log('SPRING BOOT API URL: ', springBootAPI)
 
 export default axiosInstance
